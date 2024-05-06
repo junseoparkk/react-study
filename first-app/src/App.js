35f7157b3e1f8cb79cddd2c2099a6ec4e98d8980
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-07 03:06:55 +0900

    Part 01: Props

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,40 +1,50 @@
 import './App.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1><a href="/">React</a></h1>
+      <h1 ><a href="/">{props.title}</a></h1>
     </header>
   )
 }
 
-const Nav = () => {
+const Nav = (props) => {
+  const list = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let topic = props.topics[i];
+    list.push(<li key={topic.id}><a href={`/read/${topic.id}`}>{topic.title}</a></li>);
+  }
+
   return (
     <nav>
       <ol>
-        <li><a href="/read/1">html</a></li>
-        <li><a href="/read/2">css</a></li>
-        <li><a href="/read/3">js</a></li>
+        {list}
       </ol>
     </nav>
   )
 }
 
-const Article = () => {
+const Article = ({ title, body }) => {
   return (
     <article>
-      <h2>Welcome!</h2>
-      Hello, WEB
+      <h2>{title}</h2>
+      {body}
     </article>
   )
 }
 
 const App = () => {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'},
+  ]
+
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="Title"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB~"></Article>
     </div>
   );
 }
