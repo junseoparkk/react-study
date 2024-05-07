60936c9f91660dc4e6c069e6f04584ad05373411
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-07 10:42:11 +0900

    Part 01: Create

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -41,16 +41,35 @@ const Article = ({ title, body }) => {
   )
 }
 
+const Create = (props) => {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type='text' name='title' placeholder='title'></input></p>
+        <p><textarea name='body' placeholder='body'></textarea></p>
+        <p><input type='submit' value='Create'></input></p>
+      </form>
+    </article>
+  )
+}
+
 const App = () => {
   const [mode, setMode] = useState('WELCOME');
   const [_id, setId] = useState(null);
-
-  let content = null;
-  const topics = [
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'},
-  ]
+  ])
+  const [nextId, setNextId] = useState(4);
+
+  let content = null;
 
   if (mode === 'WELCOME') {
     content = <Article title='Welcome' body='Hello, Web'></Article>
@@ -63,6 +82,17 @@ const App = () => {
       }
     }
     content = <Article title={title} body={body}></Article>
+  } else if (mode === 'CREATE') {
+      content = <Create onCreate={(_title, _body) => {
+        const newTopic = {id:nextId, title:_title, body:_body};
+        const newTopics = [...topics];
+        newTopics.push(newTopic);
+        setTopics(newTopics);
+        setMode('READ');
+        setId(nextId);
+        setNextId(nextId + 1);
+      }}>
+      </Create>
   }
 
   return (
@@ -75,6 +105,10 @@ const App = () => {
         setId(_id);
       }}></Nav>
       {content}
+      <a href='create' onClick={event => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
