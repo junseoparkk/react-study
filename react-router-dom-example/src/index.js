d005a56e3724ccd3cf4a1dc2fbb409ae908cf58a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-07 17:26:09 +0900

    Part 02: Project environment setting

--- react-router-dom-example/src/index.js
+++ react-router-dom-example/src/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  );
+};
+
+const Topics = () => {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React Router Dom!</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
